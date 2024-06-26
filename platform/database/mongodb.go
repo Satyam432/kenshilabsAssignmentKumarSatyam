771c8ecf90df93fc35fd90b3da4a1f921e5b71d2
satyam432 <kumarshatyam05@gmail.com>
2024-06-26 14:18:03 +0530

    mongo client initialized

---
+++ platform/database/mongodb.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var GlobalMongoClient *mongo.Client
+var mongoErr error
+
+func InitializeMongoDB() {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("mongodb+srv://432satyam432:vzImdy4NxdHpDIEB@cluster0.jqknrbi.mongodb.net/?appName=Cluster0").SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	GlobalMongoClient, mongoErr = mongo.Connect(context.TODO(), opts)
+	if mongoErr != nil {
+		fmt.Println("Error Initializing Mongo Client")
+	}
+}
