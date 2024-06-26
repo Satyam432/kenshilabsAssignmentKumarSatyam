3e7c822ade1097cd881a0289fd13330d37dbe5d6
satyam432 <kumarshatyam05@gmail.com>
2024-06-26 23:49:08 +0530

    auth initial commit for tasks

--- pkg/middleware/authMiddleware.go
+++ pkg/middleware/authMiddleware.go
@@ -1,1 +1,30 @@
 package middleware
+
+// import (
+// 	"github.com/gofiber/fiber/v2"
+// )
+
+// func AuthenticateUser() func(*fiber.Ctx) error {
+
+// }
+
+// func AssignAuthentication() func(*fiber.Ctx) error {
+// 	return func(c *fiber.Ctx) error {
+// 		// Define a struct to hold the request body data
+// 		userData := struct {
+// 			Password string `json:"password"`
+// 			Email    string `json:"email"`
+// 		}{}
+
+// 		// Parse the request body
+// 		if err := c.BodyParser(&userData); err != nil {
+// 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+// 		}
+
+// 		// Set the email in the context locals
+// 		c.Locals("email", userData.Email)
+
+// 		// Continue with the next handler
+// 		return c.Next()
+// 	}
+// }
