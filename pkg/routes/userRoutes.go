ccb7876c033baf34926b2d24dab1d5cbcf16c50a
satyam432 <kumarshatyam05@gmail.com>
2024-06-26 23:49:34 +0530

    user level action routs

--- pkg/routes/userRoutes.go
+++ pkg/routes/userRoutes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"example.com/m/app/controller"
+	"github.com/gofiber/fiber/v2"
+)
+
+func UserRoutes(a *fiber.App) {
+	route := a.Group("/api/v1")
+
+	route.Post("/signup", controller.UserSignUp)
+	route.Post("/signin", controller.UserSignIn)
+	route.Post("/signout", controller.UserSignOut)
+
+}
