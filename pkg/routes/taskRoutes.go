cb4e3cfa2682307f35da83bcb4b035e34ab9f744
satyam432 <kumarshatyam05@gmail.com>
2024-06-27 09:58:02 +0530

    task routes init

--- pkg/routes/taskRoutes.go
+++ pkg/routes/taskRoutes.go
@@ -1,1 +1,21 @@
 package routes
+
+import (
+	"example.com/m/app/controller"
+	"github.com/gofiber/fiber/v2"
+)
+
+func TaskRoutes(a *fiber.App) {
+	route := a.Group("/")
+
+	route.Post("/signup", controller.UserSignUp)
+	route.Post("/signin", controller.UserSignIn)
+	route.Post("/signout", controller.UserSignOut)
+
+	// Task Management Endpoints
+	route.Post("/tasks", controller.PostTask)
+	route.Get("/tasks", controller.GetTask)
+	route.Get("/tasks/:id", controller.GetTaskById)
+	route.Put("/tasks/:id", controller.UpdateTask)
+	route.Delete("/tasks/:id", controller.DeleteTask)
+}
